d38bc6108e95a1222162fe5b12eed89c082154db
repurpo <repurpo.ai@gmail.com>
2026-04-19 21:53:37 +0530

    Admin update.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import VercelInsights from "@/components/analytics";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,7 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
-      <body className="font-sans">{children}</body>
+      <body className="font-sans">
+        {children}
+        {/* Tracking stays here to cover all pages */}
+        <VercelInsights />
+      </body>
     </html>
   );
 }
